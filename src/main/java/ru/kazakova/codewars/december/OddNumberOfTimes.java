f495b50aae4cc769e0d45f5f974ae45984a81cab
VCKazakova <v.c.kazakova@gmail.com>
2023-12-07 12:17:35 +0300

    correct issue

--- src/main/java/ru/kazakova/codewars/december/OddNumberOfTimes.java
+++ src/main/java/ru/kazakova/codewars/december/OddNumberOfTimes.java
@@ -52,17 +52,46 @@ public class OddNumberOfTimes {
         return 0;
     }
 
+    public static int findIt2(int[] a) {
+        if (a.length == 1) {
+            return a[0];
+        }
+        // Сортируем массив
+        Arrays.sort(a);
+
+        int count = 1;
+
+        // Проходим по отсортированному массиву
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] == a[i + 1]) {
+                // Если текущий элемент совпадает со следующим, увеличиваем счетчик
+                count++;
+            } else {
+                // Если текущий элемент не совпадает со следующим, проверяем четность счетчика
+                if (count % 2 != 0) {
+                    // Если счетчик нечетный, значит, текущее число встречается нечетное количество раз
+                    return a[i];
+                }
+                count = 1; // Сбрасываем счетчик при переходе к новому числу
+            }
+        }
+        // Последний элемент массива
+        if (count % 2 != 0) {
+            return a[a.length - 1];
+        }
+        return 0; // Возвращаем 0, если что-то пошло не так
+    }
+
 
     public static void main(String[] args) {
-//        findIt(new int[]{20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5});
+
 //        assertEquals(-1, FindOdd.findIt(new int[]{1,1,2,-2,5,2,4,4,-1,-2,5}));
 //        assertEquals(5, FindOdd.findIt(new int[]{20,1,1,2,2,3,3,5,5,4,20,4,5}));
 //        assertEquals(10, FindOdd.findIt(new int[]{10}));
 //        assertEquals(10, FindOdd.findIt(new int[]{1,1,1,1,1,1,10,1,1,1,1}));
 //        assertEquals(1, FindOdd.findIt(new int[]{5,4,3,2,1,5,4,3,2,10,10}));
-        findIt(new int[]{1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5});
-        findIt(new int[]{20,1,1,2,2,3,3,5,5,4,20,4,5});
-        findIt(new int[]{1,1,1,1,1,1,10,1,1,1,1});
-        findIt(new int[]{5,4,3,2,1,5,4,3,2,10,10});
+
+        System.out.println(findIt2(new int[]{5,4,3,2,1,5,4,3,2,10,10}));
+        System.out.println(findIt2(new int[]{20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5}));
     }
 }
