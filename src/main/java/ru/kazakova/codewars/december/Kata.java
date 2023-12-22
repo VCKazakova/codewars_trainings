0ebe69b96a5f85e303f7ccad00fff91dcd77ee5a
VCKazakova <v.c.kazakova@gmail.com>
2023-12-22 17:56:15 +0300

    Got an issue

--- src/main/java/ru/kazakova/codewars/december/Kata.java
+++ src/main/java/ru/kazakova/codewars/december/Kata.java
@@ -1,5 +1,10 @@
 package ru.kazakova.codewars.december;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Objects.nonNull;
+
 public class Kata {
 
     /**
@@ -25,14 +30,68 @@ public class Kata {
      */
 
 
-
+    // на входе будут "youuuuu", "u", "you" - их меняем на your sister
     public static String autocorrect(String input) {
-        // your code here
-        return ""; // "corrected" input
+        if (input.contains(" ")) {
+            return autocorrectBigString(input);
+        } else {
+            return changeOrGetInputString(input) ? "your sister" : input;
+        }
+    }
+
+    public static boolean changeOrGetInputString(String input) {
+        return (((input.startsWith("y") || input.startsWith("Y"))
+                && (input.endsWith("u") || input.endsWith("U")))
+                || ("u".equalsIgnoreCase(input) || "you".equalsIgnoreCase(input)));
+    }
+
+    public static String getSignFromString(String input) {
+        String punctuationRegex = "[\\p{Punct}]";
+        Pattern pattern = Pattern.compile(punctuationRegex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
+    }
+
+    public static String autocorrectBigString(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (input.contains(" ")) {
+            String[] splitLowerInput = input.split(" ");
+            for (String partOfSplitString : splitLowerInput) {
+                String sign = getSignFromString(partOfSplitString);
+                if (nonNull(sign)) {
+                    String[] splitWithSign = partOfSplitString.split(Pattern.quote(sign));
+                    for (String partOfStringWithSign : splitWithSign) {
+                        if (changeOrGetInputString(partOfStringWithSign)) {
+                            stringBuilder.append("your sister ");
+                        }
+                    }
+                    while (stringBuilder.length() > 0 && Character.isWhitespace(
+                            stringBuilder.charAt(stringBuilder.length() - 1))) {
+                        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                    }
+                    stringBuilder.append(sign);
+                } else if (changeOrGetInputString(partOfSplitString)) {
+                    stringBuilder.append("your sister ");
+                } else {
+                    stringBuilder.append(partOfSplitString);
+                    stringBuilder.append(" ");
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(autocorrect("youuuuuuu"));
+        System.out.println(autocorrect("youtube"));
+        System.out.println(autocorrect("u"));
+        System.out.println(autocorrect("you"));
+        System.out.println(autocorrect("you got MY u"));
+        System.out.println(autocorrect("i miss you!"));
+        System.out.println(autocorrectBigString("I miss you!"));
     }
 
 }
