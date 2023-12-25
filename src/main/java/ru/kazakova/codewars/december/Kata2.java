eeb4cad7c868b399998f31f15a083314cdaa7006
VCKazakova <v.c.kazakova@gmail.com>
2023-12-25 08:43:46 +0300

    Add new task

---
+++ src/main/java/ru/kazakova/codewars/december/Kata2.java
@@ -0,0 +1,24 @@
+package ru.kazakova.codewars.december;
+
+public class Kata2 {
+
+    /**
+     * https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/java
+     * Complete the solution so that it returns true if the first argument(string)
+     * passed in ends with the 2nd argument (also a string).
+     *
+     * Examples:
+     *
+     * solution('abc', 'bc') // returns true
+     * solution('abc', 'd') // returns false
+     */
+
+    public static boolean solution(String str, String ending) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
