10ea0b13df3e0c9e12012b33e436556824c7ae44
VCKazakova <v.c.kazakova@gmail.com>
2023-12-28 18:37:46 +0300

    Add new task

---
+++ src/main/java/ru/kazakova/codewars/december/StringSplit.java
@@ -0,0 +1,25 @@
+package ru.kazakova.codewars.december;
+
+public class StringSplit {
+
+    /**
+     * https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/java
+     * Complete the solution so that it splits the string into pairs of two characters.
+     * If the string contains an odd number of characters then it should replace the missing second
+     * character of the final pair with an underscore ('_').
+     *
+     * Examples:
+     *
+     * * 'abc' =>  ['ab', 'c_']
+     * * 'abcdef' => ['ab', 'cd', 'ef']
+     */
+
+    public static String[] solution(String s) {
+       return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
