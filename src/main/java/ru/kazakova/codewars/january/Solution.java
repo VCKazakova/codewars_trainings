e43a54b6f8e5974c87416fd55d75021e42ae539b
VCKazakova <v.c.kazakova@gmail.com>
2024-01-10 16:13:38 +0300

    Add new task

---
+++ src/main/java/ru/kazakova/codewars/january/Solution.java
@@ -0,0 +1,31 @@
+package ru.kazakova.codewars.january;
+
+public class Solution {
+
+    /**
+     * https://www.codewars.com/kata/5a793fdbfd8c06d07f0000d5/train/java
+     * Consider the following expansion:
+     *
+     * solve("3(ab)") = "ababab"     -- because "ab" repeats 3 times
+     * solve("2(a3(b))") = "abbbabbb" -- because "a3(b)" == "abbb", which repeats twice.
+     * Given a string, return the expansion of that string.
+     *
+     * Input will consist of only lowercase letters and numbers (1 to 9) in valid parenthesis.
+     * There will be no letters or numbers after the last closing parenthesis.
+     *
+     * More examples in test cases.
+     *
+     * Good luck!
+     *
+     * Please also try Simple time difference
+     */
+
+    public static String solve(String s){
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
