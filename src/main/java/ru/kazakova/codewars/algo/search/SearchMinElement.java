13ef43813211911df6e2d2277f09144546f07899
VCKazakova <v.c.kazakova@gmail.com>
2024-01-11 14:20:41 +0300

    Learning search of min element

---
+++ src/main/java/ru/kazakova/codewars/algo/search/SearchMinElement.java
@@ -0,0 +1,13 @@
+package ru.kazakova.codewars.algo.search;
+
+public class SearchMinElement {
+
+    public int getMinElementFromArray(int [] intArray) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
