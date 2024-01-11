d58af093df276f38bd3df7d6805870616ea22b57
VCKazakova <v.c.kazakova@gmail.com>
2024-01-11 14:58:21 +0300

    Learning sorting by choose

--- src/main/java/ru/kazakova/codewars/algo/sorting/SortingByChoose.java
+++ src/main/java/ru/kazakova/codewars/algo/sorting/SortingByChoose.java
@@ -1,5 +1,7 @@
 package ru.kazakova.codewars.algo.sorting;
 
+import java.util.Arrays;
+
 public class SortingByChoose {
 
     /**
@@ -7,7 +9,6 @@ public class SortingByChoose {
      * на каждом шаге происходит поиск минимального элемента в массиве
      * минимальный элемент меняется местами с первым элементом
      * следующие шаги происходят без учета отсортированных элементов
-     *
      */
 
     public static int getMinIndexFromPartOfArray(int [] intArray, int startIndex) {
@@ -22,10 +23,24 @@ public class SortingByChoose {
         return minIndex;
     }
 
+    public static int[] sortingArrayByChose(int[] intArray) {
+        for (int step = 0; step < intArray.length; step++) {
+            // поиск индекса с мин элементом в части массива
+            int index = getMinIndexFromPartOfArray(intArray, step);
+            // временная переменная для хранения элемента под номером шага
+            int tmp = intArray[step];
+            // элемент меняется местами с минимальным
+            intArray[step] = intArray[index];
+            intArray[index] = tmp;
+        }
+        return intArray;
+    }
+
     public static void main(String[] args) {
         int[] intArray = new int[]{77, 9, 17, 39, 2, 6, 8, 1, 0, 15};
         int[] intArray2 = new int[]{4, 6, 2, 8, 1, 9, 23, 67};
         System.out.println(getMinIndexFromPartOfArray(intArray2, 3));
+        System.out.println(Arrays.toString(sortingArrayByChose(intArray)));
     }
 
 }
