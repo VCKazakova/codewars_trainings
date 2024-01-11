0b7c02d88d5a18f86ba74dca4f0670cf7a760d6a
VCKazakova <v.c.kazakova@gmail.com>
2024-01-11 14:18:39 +0300

    Add notes

--- src/main/java/ru/kazakova/codewars/january/Solution.java
+++ src/main/java/ru/kazakova/codewars/january/Solution.java
@@ -1,5 +1,7 @@
 package ru.kazakova.codewars.january;
 
+import java.util.Stack;
+
 public class Solution {
 
     /**
@@ -20,12 +22,35 @@ public class Solution {
      * Please also try Simple time difference
      */
 
-    public static String solve(String s){
-        return null;
+    public static String solve(String s) {
+
+//        Инициализация: Создайте пустой стек для хранения символов.
+//        Обход строки: Пройдитесь по каждому символу во входной строке.
+//                Обработка символов:
+//        Если текущий символ - буква, добавьте её во временную строку.
+//        Если текущий символ - цифра, обновите повторяющуюся подстроку.
+//        Если текущий символ - открывающая скобка, положите текущую временную строку и повторяющуюся подстроку в стек,
+//        затем сбросьте временную строку и повторяющуюся подстроку.
+//        Если текущий символ - закрывающая скобка, извлеките последнюю временную строку и повторяющуюся подстроку из стека,
+//        затем обновите временную строку.
+//        Формирование результата: Сформируйте итоговую строку на основе временной строки и повторяющейся подстроки.
+
+        Stack<String> stack = new Stack<>();
+        StringBuilder notConstantString = new StringBuilder();
+        StringBuilder repeatableString = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char elem = s.charAt(i);
+            if (Character.isAlphabetic(elem)) {
+                notConstantString.append(elem);
+            } else if (Character.isDigit(elem)) {
+
+            }
+        }
+        return s;
     }
 
     public static void main(String[] args) {
-
+        solve("k(a3(b(a2(c))))");
     }
 
 }
