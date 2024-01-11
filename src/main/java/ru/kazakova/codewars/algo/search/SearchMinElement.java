2f6aea7d1f0caaa30030e65aef1b9253a7dac2a3
VCKazakova <v.c.kazakova@gmail.com>
2024-01-11 14:26:15 +0300

    Add an issue

--- src/main/java/ru/kazakova/codewars/algo/search/SearchMinElement.java
+++ src/main/java/ru/kazakova/codewars/algo/search/SearchMinElement.java
@@ -2,12 +2,26 @@ package ru.kazakova.codewars.algo.search;
 
 public class SearchMinElement {
 
-    public int getMinElementFromArray(int [] intArray) {
-        return 0;
+    /**
+     * присвоить минимальное значение из 1 элемента
+     * итерируемся с 1 элемента
+     * если текущий элемент меньше 1, переписать значение
+     */
+
+    public static int getMinElementFromArray(int[] intArray) {
+        int minElem = intArray[0];
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] < minElem) {
+                minElem = intArray[i];
+            }
+        }
+        return minElem;
     }
 
     public static void main(String[] args) {
-
+        int[] intArray = new int[]{77, 9, 17, 39, 2, 6, 8, 1, 0, 15};
+        int[] intArray2 = new int[]{4, 6, 2, 8, 1, 9, 23, 67};
+        System.out.println(getMinElementFromArray(intArray2));
     }
 
 }
