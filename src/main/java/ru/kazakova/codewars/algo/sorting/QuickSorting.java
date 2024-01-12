622b320c0086358f54062afa91911726e2ffb1e7
VCKazakova <v.c.kazakova@gmail.com>
2024-01-12 15:09:56 +0300

    Define exit and entry parameters

--- src/main/java/ru/kazakova/codewars/algo/sorting/QuickSorting.java
+++ src/main/java/ru/kazakova/codewars/algo/sorting/QuickSorting.java
@@ -2,12 +2,22 @@ package ru.kazakova.codewars.algo.sorting;
 
 public class QuickSorting {
 
-    public static int[] getSortingArray(int[] ints) {
+    /**
+     * данный вид сортировки основан на выборе опорного элемента
+     * делении массива по опорному элементу
+     * создание подмассива меньше опорного элемента
+     * создание подмассива больше опорного элемента
+     * операция рекурсивна
+     */
+    public static int[] getSortingArray(int[] ints, int low, int high) {
+        if (ints.length < 2) {
+            return ints;
+        }
         return null;
     }
 
     public static void main(String[] args) {
-
+        int[] ints = new int[]{17, 29, 0, 39, 10};
     }
 
 }
