535f6f93546cfd564b0a25b2cc308581b48ed81e
VCKazakova <v.c.kazakova@gmail.com>
2024-01-12 14:58:59 +0300

    Learning quick sorting

---
+++ src/main/java/ru/kazakova/codewars/algo/sorting/QuickSorting.java
@@ -0,0 +1,13 @@
+package ru.kazakova.codewars.algo.sorting;
+
+public class QuickSorting {
+
+    public static int[] getSortingArray(int[] ints) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
