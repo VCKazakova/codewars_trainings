996420924e01d800af33834fc1e73ec14dbfe5fe
VCKazakova <v.c.kazakova@gmail.com>
2024-01-12 13:51:59 +0300

    Add an issue of array sum

--- src/main/java/ru/kazakova/codewars/algo/Recursion.java
+++ src/main/java/ru/kazakova/codewars/algo/Recursion.java
@@ -18,14 +18,28 @@ public class Recursion {
     /**
      * Рекурсивная функцию для вычисления суммы
      * элементов целочисленного массива.
+     * на вход передаем массив и число интераций - оно при первом проходе = длине массива
+     * каждый раз уменьшаем итерацию на 1 и складываем с предыдущим результатом
      */
-    public static long sumOfArrayElements(int[] ints) {
-        return 0;
+    public static long sumOfArrayElements(int[] ints, int iter) {
+        if (iter == 0) {
+            return 0;
+        }
+        return ints[iter - 1] + sumOfArrayElements(ints, iter - 1);
     }
 
     public static void main(String[] args) {
         System.out.println(getFactorial(1));
         System.out.println(getFactorial(5));
+
+        int[] ints = new int[]{1, 2, 3};
+        System.out.println(sumOfArrayElements(ints, ints.length));
+
+        int[] ints2 = new int[]{5, 2, 10};
+        System.out.println(sumOfArrayElements(ints2, ints.length));
+
+        int[] ints3 = new int[]{9, 12, 20};
+        System.out.println(sumOfArrayElements(ints3, ints.length));
     }
 
 
