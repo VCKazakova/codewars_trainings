5058767d51bfe2cbdf8879616f9ff45cd234a942
VCKazakova <v.c.kazakova@gmail.com>
2024-01-18 12:44:18 +0300

    Add new task

---
+++ src/main/java/ru/kazakova/codewars/january/Scramblies.java
@@ -0,0 +1,30 @@
+package ru.kazakova.codewars.january;
+
+public class Scramblies {
+
+    /**
+     * https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/java
+     * Complete the function scramble(str1, str2) that returns true if a portion of str1
+     * characters can be rearranged to match str2, otherwise returns false.
+     * <p>
+     * Notes:
+     * <p>
+     * Only lower case letters will be used (a-z). No punctuation or digits will be included.
+     * Performance needs to be considered.
+     * Examples
+     * scramble('rkqodlw', 'world') ==> True
+     * scramble('cedewaraaossoqqyt', 'codewars') ==> True
+     * scramble('katas', 'steak') ==> False
+     */
+
+    public static boolean scramble(String str1, String str2) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(scramble("rkqodlw", "world"));
+    }
+
+
+}
