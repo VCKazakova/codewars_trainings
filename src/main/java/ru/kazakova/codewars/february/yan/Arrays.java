6a4913509c1dde7115b1c8bc34aae25244777db9
VCKazakova <v.c.kazakova@gmail.com>
2024-02-12 12:17:20 +0300

    Training uniq value

--- src/main/java/ru/kazakova/codewars/february/yan/Arrays.java
+++ src/main/java/ru/kazakova/codewars/february/yan/Arrays.java
@@ -7,4 +7,26 @@ public class Arrays {
      * и лишь одно один раз - найти это число
      */
 
+    public static void main(String[] args) {
+        System.out.println(findInt(new int[]{
+                1, 14, 7, 5, 1, 7, 5
+        }));
+        System.out.println(findInt(new int[]{
+                4, 4, 6, 8, 9, 9, 8
+        }));
+    }
+
+    public static int findInt(int[] intArray) {
+        int[] ints = java.util.Arrays.stream(intArray)
+                .sorted()
+                .toArray();
+        for (int i = 0; i < ints.length - 1; i++) {
+            if (ints[i] != ints[i + 1]) {
+                return ints[i];
+            }
+            i++;
+        }
+        return ints[ints.length - 1];
+    }
+
 }
