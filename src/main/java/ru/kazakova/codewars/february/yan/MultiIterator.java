c06ee3b2aae5165bc64e30dedc24a7683b4a5c2c
VCKazakova <v.c.kazakova@gmail.com>
2024-02-12 13:21:02 +0300

    Add multiiterator

--- src/main/java/ru/kazakova/codewars/february/yan/MultiIterator.java
+++ src/main/java/ru/kazakova/codewars/february/yan/MultiIterator.java
@@ -16,11 +16,45 @@ public class MultiIterator {
         list.add(2);
         list.add(3);
 
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+
         Iterator<Integer> iterator = list.listIterator();
 
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
+
+        MultiIteratorInner multiIteratorInner = new MultiIteratorInner(list.iterator(), list2.listIterator());
+        multiIteratorInner.iterateBoth();
+    }
+
+    static class MultiIteratorInner {
+
+        Iterator<?> iterator;
+        Iterator<?> iterator2;
+
+        MultiIteratorInner(Iterator<?> iterator, Iterator<?> iterator2) {
+            this.iterator = iterator;
+            this.iterator2 = iterator2;
+        }
+
+        /**
+         * если хотим вывести попорядку - используем while
+         */
+        public void iterateBoth() {
+            while (iterator.hasNext() || iterator2.hasNext()) {
+                if (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+                if (iterator2.hasNext()) {
+                    System.out.println(iterator2.next());
+                }
+            }
+        }
+
     }
 
 }
