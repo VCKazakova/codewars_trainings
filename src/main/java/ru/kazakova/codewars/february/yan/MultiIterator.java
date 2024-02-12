c874697b1ff096be3d5cad6f5b12c9c2e184208a
VCKazakova <v.c.kazakova@gmail.com>
2024-02-12 13:13:36 +0300

    Add iterator

--- src/main/java/ru/kazakova/codewars/february/yan/MultiIterator.java
+++ src/main/java/ru/kazakova/codewars/february/yan/MultiIterator.java
@@ -1,9 +1,26 @@
 package ru.kazakova.codewars.february.yan;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MultiIterator {
 
     /**
      * реализовать multiiterator, который содежит в себе 2 итератора
      */
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> iterator = list.listIterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 
 }
